bea8186b1800ccf1a0b2ccbd5d6835d2022f2279
al1-ce <al1-ce@null.net>
2025-06-10 04:51:45 +0300

    remove aaaaaa from intro

--- plugin/despair/intro.lua
+++ plugin/despair/intro.lua
@@ -5,8 +5,6 @@ local executed
 if vim.g.intro_executed ~= nil then return end
 vim.g.intro_executed = true
 
-vim.notify("aaaaaa")
-
 -- Disable default intro message
 vim.opt.shortmess:append('I')
 
